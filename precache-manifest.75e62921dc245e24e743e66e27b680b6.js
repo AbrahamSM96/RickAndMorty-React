ee5c65924273332218492b656ed1c63a24bcaa7a
AbrahamSM96 <abrahamserrano96@hotmail.com>
2020-05-17 02:07:41 -0500

    Updates

---
+++ precache-manifest.75e62921dc245e24e743e66e27b680b6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96946083624a2e1a6ec0bc45cecbbeb3",
+    "url": "/AbrahamSM96/RickAndMorty-React/index.html"
+  },
+  {
+    "revision": "758c8ffb8c6078c03c1c",
+    "url": "/AbrahamSM96/RickAndMorty-React/static/css/2.4593731b.chunk.css"
+  },
+  {
+    "revision": "def744f0a8202e511e11",
+    "url": "/AbrahamSM96/RickAndMorty-React/static/css/main.86ca4b3d.chunk.css"
+  },
+  {
+    "revision": "758c8ffb8c6078c03c1c",
+    "url": "/AbrahamSM96/RickAndMorty-React/static/js/2.7c107e76.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/AbrahamSM96/RickAndMorty-React/static/js/2.7c107e76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "def744f0a8202e511e11",
+    "url": "/AbrahamSM96/RickAndMorty-React/static/js/main.06cbd262.chunk.js"
+  },
+  {
+    "revision": "fb575d7aeb2a64770c97",
+    "url": "/AbrahamSM96/RickAndMorty-React/static/js/runtime-main.0b21077e.js"
+  },
+  {
+    "revision": "6ae59feb4fcb8358c8c4a184d32f1bb5",
+    "url": "/AbrahamSM96/RickAndMorty-React/static/media/background-stars.6ae59feb.jpg"
+  },
+  {
+    "revision": "baf45affd349b64426cf3de7ecb813b0",
+    "url": "/AbrahamSM96/RickAndMorty-React/static/media/lupa.baf45aff.svg"
+  },
+  {
+    "revision": "ce98be6381dab9a129e09ba3c4ff307e",
+    "url": "/AbrahamSM96/RickAndMorty-React/static/media/rick-and-morty-family-png-5-transparent.ce98be63.png"
+  },
+  {
+    "revision": "91db29e986ea72e029a2ae2b46531461",
+    "url": "/AbrahamSM96/RickAndMorty-React/static/media/shlop_rg.91db29e9.ttf"
+  }
+]);
